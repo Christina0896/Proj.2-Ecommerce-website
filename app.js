4d296b1cafc2b8c83fd3f1bec803f35e025d1351
Cristina <cristinabandeira82@gmail.com>
2024-12-03 23:00:52 +0000

    Info Bar_Dark Mode

--- app.js
+++ app.js
@@ -29,3 +29,28 @@ const countdown = setInterval(() => {
     clearInterval(countdown);
   }
 }, 1000);
+
+//Info Bar//
+
+//Dark Mode//
+const themeSwitch = document.getElementById("theme_switch");
+
+// Check and apply dark mode from localStorage
+if (localStorage.getItem("dark_mode") === "active") {
+  document.body.classList.add("dark_mode");
+}
+// Toggle dark mode on button click
+themeSwitch.addEventListener("click", () => {
+  // Toggle the dark_mode class
+  document.body.classList.toggle("dark_mode");
+
+  // Check if dark mode is active
+  const isDarkMode = document.body.classList.contains("dark_mode");
+
+  // Update localStorage based on the current state
+  if (isDarkMode) {
+    localStorage.setItem("dark_mode", "active");
+  } else {
+    localStorage.setItem("dark_mode", "inactive");
+  }
+});
